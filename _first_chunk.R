85798f4a1cc56cfaf2a4254f78941ef20c3d40e9
paulsharpeY <paulsharpe@miraclefish.com>
2021-05-12 09:46:44 +0100

    Apply stylr

--- _first_chunk.R
+++ _first_chunk.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(pander)
 library(webex)
-knitr::opts_chunk$set(echo = TRUE,
-                      collapse=TRUE,
-                      comment=NA,
-                      cache = TRUE,
-                      message=FALSE)
+knitr::opts_chunk$set(
+  echo = TRUE,
+  collapse = TRUE,
+  comment = NA,
+  cache = TRUE,
+  message = FALSE
+)
