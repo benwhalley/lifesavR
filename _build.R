5256e1dccc981c0feec0a41786d9d0c1dc4141a8
benwhalley <ben.whalley@plymouth.ac.uk>
2021-08-19 11:56:25 +0100

    add build script

---
+++ _build.R
@@ -0,0 +1,9 @@
+setwd("~/dev/lifesavr/")
+
+
+rmarkdown::render('index.rmd')
+rmarkdown::render('session-1.rmd')
+rmarkdown::render('session-2.rmd')
+rmarkdown::render('session-4.rmd')
+rmarkdown::render('session-4.rmd')
+rmarkdown::render('session-5.rmd')
